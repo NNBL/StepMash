644776fcf0a6b19898891cd35becac024a1d85d2
Henrik Width <henrik.width@gmail.com>
2014-03-02 22:08:04 +0100

    Resonable docstrings

--- StepMashClass.py
+++ StepMashClass.py
@@ -1,8 +1,10 @@
-"""docstring here"""
+"""Class to calculate temperature and amount 
+of water needed for step mashing. 
+Copyright 2014 hwidth NNBL"""
 
 
 class StepMash(object):
-    """docstring here"""
+    """Add mash temperature steps, get amount and temp of water needed"""
     def __init__(self):
         self.r = 2.5  # ratio of water in liter per kg
         self.steps = [22, ]  # Initial temp of the mash (start at room temp)
@@ -13,19 +15,21 @@ class StepMash(object):
         self.tc = 0.41  # Termodynamic constant in Celsius
 
     def setGrainweight(self, weight):
-        """docstring here"""
+        """Set the weight of grain/grits in mash"""
         self.G = weight
 
     def setGraintemp(self, temp):
-        """docstring here"""
+        """Set the initial temperature of the grain
+        usually room temperature"""
         self.steps[0] = temp
 
     def setWatertemp(self, temp):
-        """docstring here"""
+        """Set if using other temperature than 99C"""
         self.Temp_of_added_water = temp
 
     def addStep(self, step):
-        """docstring here"""
+        """Add mash temperature in increasing steps
+        e.g 22, 55, 64, 78"""
         if len(self.steps) == 1:
             strikewater = self.temp_of_added_water(self.steps[0], step)
             self.watertemp.append(round(strikewater, 1))
@@ -40,12 +44,12 @@ class StepMash(object):
         self.steps.append(step)
 
     def temp_of_added_water(self, T1, T2):
-        """docstring here"""
+        """Calculate the temperature of initial mash in"""
         Tw = (self.tc/self.r)*(T2-T1)+T2  # Temperature of added water
         return Tw
 
     def __str__(self):
-        """docstring here"""
+        """Pretty-print the info"""
         return "Start temperature: %sC\nGrain in mash: %sKg\nLiter per kilo grain ratio: %sl\n" % (self.steps[0], self.G, self.r)
 
 if __name__ == "__main__":
