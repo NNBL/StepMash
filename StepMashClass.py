0537b922844addea64e30407a4d2768aa90db083
Henrik Width <henrik.width@gmail.com>
2014-03-02 21:53:03 +0100

    pylint and pep8 fixes

--- StepMashClass.py
+++ StepMashClass.py
@@ -1,38 +1,51 @@
-class StepMash():
+"""docstring here"""
+
+
+class StepMash(object):
+    """docstring here"""
     def __init__(self):
-        self.Wm = 0 # Total amount of water in mash
-        self.r = 2.5 #ratio of water in liter per kg
-        self.steps = [22,] # Initial temperature of the mash (start at room temp)
+        self.r = 2.5  # ratio of water in liter per kg
+        self.steps = [22, ]  # Initial temp of the mash (start at room temp)
         self.wateramount = []
         self.watertemp = []
-        self.Temp_of_added_water = 99.0 # Temperature of added water in celsius (assume near boiling)
-        self.G = 2.5 #Amount of grain in mash in kilos
-        self.tc = 0.41 #Termodynamic constant in Celsius
+        self.Temp_of_added_water = 99.0  # Temp of added water in celsius
+        self.G = 2.5  # Amount of grain in mash in kilos
+        self.tc = 0.41  # Termodynamic constant in Celsius
 
     def setGrainweight(self, weight):
+        """docstring here"""
         self.G = weight
 
     def setGraintemp(self, temp):
+        """docstring here"""
         self.steps[0] = temp
 
     def setWatertemp(self, temp):
+        """docstring here"""
         self.Temp_of_added_water = temp
 
     def addStep(self, step):
+        """docstring here"""
         if len(self.steps) == 1:
             strikewater = self.temp_of_added_water(self.steps[0], step)
             self.watertemp.append(round(strikewater, 1))
-            self.wateramount.append(round((step-self.steps[0])*(self.tc*self.G+sum(self.wateramount))/(strikewater-step), 2)) # Amount of added water
+            self.wateramount.append(round((step - self.steps[0]) *
+                                        (self.tc * self.G + sum(self.wateramount)) /
+                                        (strikewater-step), 2))
         else:
             self.watertemp.append(self.Temp_of_added_water)
-            self.wateramount.append(round((step-self.steps[-1])*(self.tc*self.G+sum(self.wateramount))/(self.Temp_of_added_water-step), 2)) # Amount of water added
+            self.wateramount.append(round((step - self.steps[-1]) *
+                                         (self.tc * self.G + sum(self.wateramount)) /
+                                         (self.Temp_of_added_water - step), 2))
         self.steps.append(step)
 
     def temp_of_added_water(self, T1, T2):
-        Tw = (self.tc/self.r)*(T2-T1)+T2 # Temperature of added water
+        """docstring here"""
+        Tw = (self.tc/self.r)*(T2-T1)+T2  # Temperature of added water
         return Tw
 
     def __str__(self):
+        """docstring here"""
         return "Start temperature: %sC\nGrain in mash: %sKg\nLiter per kilo grain ratio: %sl\n" % (self.steps[0], self.G, self.r)
 
 if __name__ == "__main__":
