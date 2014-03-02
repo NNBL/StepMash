8de619aed6f7fdd458f8c74887d0650632552b95
Henrik Width <henrik.width@gmail.com>
2014-03-02 23:16:33 +0100

    sample script to test StepMash class

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ if __name__ == "__main__":
     myMash.setGrainweight(5.6)
     myMash.setGraintemp(24)
 
-    myMash.addStep(55)
+    myMash.addStep(50)
     myMash.addStep(68)
     myMash.addStep(78)
+    for i in xrange(len(myMash.steps)):
+        if i == 0:
+            pl.plot(i,myMash.steps[i],"o")
+        else:
+            label = str(myMash.wateramount[i-1])+"L @ "+str(myMash.watertemp[i-1])+"C"
+            pl.plot(i,myMash.steps[i],"o", label=label)
+    pl.title("Step mash water schedule")
+    pl.xlabel("step #")
+    pl.ylabel("temperature")
+    pl.grid()
+    pl.legend(loc="lower right")
+    pl.show()
